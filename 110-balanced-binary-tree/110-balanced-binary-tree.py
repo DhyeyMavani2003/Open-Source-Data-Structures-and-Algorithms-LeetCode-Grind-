6f84f9666bab3f3b1a98108d1a8b445147846c32
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2022-06-20 15:06:25 -0400

    Time: 94 ms (27.76%), Space: 18.7 MB (61.43%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -23,3 +23,32 @@ class Solution:
             and self.isBalanced(root.left) \
             and self.isBalanced(root.right)
         
+        
+        
+    # OR
+    
+    
+    
+    
+    # Recursively determine if a binary tree is balanced
+    # Visits each node once: O(n) time, O(h) space
+    def isBalanced(self, root):
+        # Find the height of a tree given its root
+        # return -1 if unbalanced
+        def height(root):
+            # Base case
+            if root is None:
+                return 0
+
+            left_height  = height(root.left)
+            right_height = height(root.right)
+
+            # If tree is unbalanced, return -1
+            if left_height == -1 or right_height == -1 or \
+                abs(left_height - right_height) > 1:
+                return -1
+
+            # Otherwise, return height of tree
+            return 1 + max(left_height, right_height)
+
+        return height(root) != -1
