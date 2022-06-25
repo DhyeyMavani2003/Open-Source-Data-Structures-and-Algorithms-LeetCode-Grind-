a8e54d06a68da4f32dfc6ed18bdab25555e776d3
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2022-06-24 22:34:44 -0400

    Time: 56 ms (71.14%), Space: 16.2 MB (95.47%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -13,4 +13,20 @@ class Solution:
             return (valid(node.left,left,node.val) and
                     valid(node.right,node.val,right))
         return valid(root,float("-inf"),float("inf"))
+    
+    def isValidBST(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+
+        stack = [(root, -math.inf, math.inf)]
+        while stack:
+            root, lower, upper = stack.pop()
+            if not root:
+                continue
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+            stack.append((root.right, val, upper))
+            stack.append((root.left, lower, val))
+        return True
         
