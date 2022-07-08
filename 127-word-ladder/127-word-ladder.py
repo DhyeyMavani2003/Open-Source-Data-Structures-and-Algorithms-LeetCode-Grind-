9165c4bf28bae55acec4f5905ef316da81a8635b
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2022-07-07 21:07:24 -0400

    Time: 556 ms (37.60%), Space: 15 MB (82.79%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,21 +1,20 @@
 class Solution:
-    
-
-    def ladderLength(self, start, end, words):
-        LETTERS = set('abcdefghijklmnopqrstuvwxyz')
-        words = set(words)
-        visited = set()
-        queue = [(start, 1)] # queue for BFS, which stores the word and distance
-
-        while len(queue) > 0:
-            word, length = queue.pop(0)
-            if word == end:
-                return length
-            for i, char in enumerate(word):
-                for letter in LETTERS:
-                    if char != letter:
-                        candidate = word[:i] + letter + word[i + 1:]
-                        if candidate in words and candidate not in visited:
-                            queue.append([candidate, length + 1])
-                            visited.add(candidate)
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        alphabet = set('abcdefghijklmnopqrstuvwxyz')
+        wordSet = set(wordList) # ["hot","dot","dog","lot","log","cog"]
+        visitedSet = set() # [hot, dot, dog, cog]
+        queue = [(beginWord,1)] # [(hit,1), (hot,2), (dot, 3), (dog, 4), (cog, 5) ]
+        
+        while queue:
+            curWord, curLength = queue.pop(0)
+            if curWord == endWord:
+                return curLength
+            for index, character in enumerate(curWord):
+                for letter in alphabet:
+                    if character != letter:
+                        possibleWord = curWord[:index] + letter + curWord[index+1:]
+                        if possibleWord in wordSet and possibleWord not in visitedSet:
+                            queue.append((possibleWord, curLength + 1))
+                            visitedSet.add(possibleWord)
+        
         return 0
