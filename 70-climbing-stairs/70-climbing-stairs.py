216cc193b0bb0b189413b26bbd820ef436558d6b
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2022-07-25 14:35:42 -0400

    Time: 51 ms (35.13%), Space: 13.9 MB (56.86%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    # dp[i] = the number of distinct ways to reach the top of i steps
+    # Observe that dp[i] = dp[i - 1] + dp[i - 2] (Understand why)
+    # O(n) time, O(1) space
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # dp_2 = dp[i-2], dp_1 = d[i-1]
+        dp_2 = dp_1 = 1
+        for _ in range(2, n+1):
+            dp_2, dp_1 = dp_1, dp_2 + dp_1
+        return dp_1
