5ff136cd8f2e14873ae2e092505a0453d02d8d1e
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2022-07-29 20:51:00 -0400

    Time: 38 ms (84.18%), Space: 14.2 MB (19.93%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,4 +1,5 @@
 class Solution:
+    # DP Approach
     def numDecodings(self, s: str) -> int:
         dp = {len(s) :  1}
         for i in range(len(s) - 1, -1, -1):
@@ -9,4 +10,21 @@ class Solution:
             if (i+1<len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456")):
                 dp[i] += dp[i+2]
         return dp[0]
+    
+    # DFS Approach
+    def numDecodings(self, s: str) -> int:
+        dp = {len(s) :  1}
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if s[i] == "0":
+                return 0
+            res = dfs(i+1)
+            
+            if (i+1<len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456")):
+                res += dfs(i+2)
+                
+            dp[i] = res
+            return res
+        return dfs(0)
                 
