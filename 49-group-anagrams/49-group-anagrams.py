083b909a8f41686f7364cc5703bb84ab12f76614
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2022-08-22 21:23:30 -0400

    Time: 202 ms (22.97%), Space: 18.5 MB (41.45%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,5 +1,5 @@
 class Solution:
-    '''
+    
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
         # using defaultdict allows us to not worry about the empty edge cases
@@ -13,7 +13,7 @@ class Solution:
             result[tuple(count)].append(s)
         
         return result.values()
-    '''    
+    
     def groupAnagrams(self, strs):
         result = {}
         for s in strs:
@@ -21,5 +21,6 @@ class Solution:
                 result[str(sorted(s))].append(s)
             else:
                 result[str(sorted(s))] = [s]
+        print(result.keys())
         return result.values()
         
