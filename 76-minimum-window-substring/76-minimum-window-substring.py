6d744f3aacf05fd90a3a4b6ac98cbf4f835770da
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2022-08-23 17:01:10 -0400

    Time: 92 ms (96.67%), Space: 14.6 MB (83.88%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,21 +1,15 @@
 class Solution:
     def minWindow(self, s, t):
-        if t == "":
-            return ""
-
+        if t == "": return ""
         target = {}
         for c in t:
-            if c in target:
-                target[c] += 1
-            else:
-                target[c] = 1
+            if c in target: target[c] += 1
+            else:   target[c] = 1
 
         left = 0
         current = {}
-        have = 0 
-        need = len(target)
-        res = [-1, -1]
-        resLen = float("infinity")
+        have, need = 0, len(target)
+        res, resLen = [-1, -1], float("infinity")
 
         for right in range(len(s)):
             character = s[right]
@@ -37,26 +31,4 @@ class Solution:
         finalLeft, finalRight = res
         return s[finalLeft : finalRight + 1] if resLen != float("infinity") else ""
 
-    '''
-    def minWindow(self, s: str, t: str) -> str:
-        if t == "": return ""
-        countT, window = {},{}
-        for c in t: 
-            countT[c] = 1 + countT.get(c,0)
-        have, need, l, res, resLen = 0, len(countT), 0, [-1,-1], float("infinity")
-        for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c,0)
-            if c in countT and window[c] == countT[c]:
-                have += 1
-            while have == need:
-                if (r-l+1) < resLen:
-                    res = [l,r]
-                    resLen = r-l+1
-                window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
-                    have -= 1
-                l += 1
-        finalLeft, finalRight = res
-        return s[finalLeft:finalRight+1] if resLen != float("infinity") else ""
-    '''   
+    
