e7da88decf942989fddfa55307c28b2bd3a06b83
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2022-08-24 16:14:04 -0400

    Time: 53 ms (57.46%), Space: 14.1 MB (77.53%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,8 +1,35 @@
 class Solution:
+    
+    # Neetcode solution
+    def generateParenthesis(self, n):
+        # only add open parenthesis if open < n
+        # only add a closing parenthesis if closed < open
+        # valid IIF oppen == closed == n
+        
+        stack, res = [], []
+        
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+        
+        backtrack(0, 0)
+        return res
+    
+    '''
+    # CodePath solution
     def generateParenthesis(self, n: int) -> List[str]:
         
   
-
         def recursiveCall(result,str,open,close,n):
           if open == n and close == n:
             result.append(str)
@@ -17,3 +44,4 @@ class Solution:
         result = []
         recursiveCall(result,"",0,0,n)
         return result
+    '''
