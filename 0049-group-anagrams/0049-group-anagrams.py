16f035c8adb33688e808a282e3ef825d9343bd41
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2023-05-23 17:46:31 -0700

    Time: 133 ms (16.87%), Space: 20.4 MB (27.28%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,28 @@
+class Solution:
+    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        # using defaultdict allows us to not worry about the empty edge cases
+        result = defaultdict(list)
+        
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            # python doesn't allow list as key
+            result[tuple(count)].append(s)
+        
+        return result.values()
+    
+    def groupAnagrams(self, strs):
+        result = {}
+        for s in strs:
+            if str(sorted(s)) in result:
+                result[str(sorted(s))].append(s)
+            else:
+                result[str(sorted(s))] = [s]
+        
+        return result.values()
+        
+        
+        
